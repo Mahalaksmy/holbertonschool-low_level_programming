48a865c175fa4f56104561e0de69696da7f2d70c
MahaLaksmy <3354@holbertonschool.com>
2021-06-17 16:49:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-int main()
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    char string[40];
-    strcpy(str, "Programming is like building a multilingual puzzle,\n");
-    puts(string);
-    return 0;
-    }
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
