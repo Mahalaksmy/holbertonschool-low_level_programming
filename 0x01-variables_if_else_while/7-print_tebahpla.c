7c3da440998fa81f72bd02a2011689ebe1b6d071
MahaLaksmy <3354@holbertonschool.com>
2021-06-18 13:14:38 -0700

    Alpha lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int Alpha = 122;
+
+while (Alpha > 96)
+{
+putchar(Alpha);
+Alpha--;
+}
+putchar ('\n');
+return (0);
+}
