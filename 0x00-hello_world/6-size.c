feff2fe3602f7041bd9a97d12985b7245b815e00
MahaLaksmy <3354@holbertonschool.com>
2021-06-17 17:13:55 -0700

    comando size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+printf("%lu\n", sizeof(char));
+printf("%lu\n", sizeof(int));
+printf("%lu", sizeof(long int));
+printf("%lu", sizeof(long long int));
+printf("%lu\n", sizeof(float));
+return 0;
+}
