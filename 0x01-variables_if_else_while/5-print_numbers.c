1028cf9b866f3896034a3ddebf56ae547f384063
MahaLaksmy <3354@holbertonschool.com>
2021-06-18 14:27:14 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int Alpha = 0;
+
+while (Alpha < 10)
+{
+printf("%d", Alpha);
+Alpha++;
+}
+return (0);
+}
