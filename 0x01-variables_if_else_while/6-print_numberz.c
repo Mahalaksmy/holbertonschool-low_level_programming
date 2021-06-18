bc6fe56a94ac7a675af38b16d8f1977abebd4cd6
MahaLaksmy <3354@holbertonschool.com>
2021-06-18 14:44:22 -0700

    putchar numers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int Alpha = 48;
+
+while (Alpha < 58)
+{
+putchar(Alpha);
+Alpha++;
+}
+return (0);
+}
