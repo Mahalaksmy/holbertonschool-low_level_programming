97e63fe1ddd0d83ae75677ae5e0914ed0d3ebe6d
MahaLaksmy <3354@holbertonschool.com>
2021-06-19 07:37:40 -0700

    command while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ /**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+int Alpha = 97,
+Alpha2 = 102,
+Alpha3 = 114;
+
+while (Alpha <= 100)
+{
+putchar(Alpha);
+Alpha++;
+}
+while (Alpha2 <= 112)
+{
+putchar(Alpha2);
+Alpha2++;
+}
+while (Alpha3 <= 122)
+{
+putchar(Alpha3);
+Alpha3++;
+}
+putchar ('\n');
+return (0);
+}
