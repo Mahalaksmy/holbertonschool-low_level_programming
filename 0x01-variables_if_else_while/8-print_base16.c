f4e2815bfde79aa1d78c84d35ebe3e9f030aed00
MahaLaksmy <3354@holbertonschool.com>
2021-06-20 14:44:16 -0700

    based16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int Alpha = 48,
+Alpha2 = 97;
+
+while (Alpha <= 57)
+{
+putchar(Alpha);
+Alpha++;
+}
+while (Alpha2 <= 102)
+{
+putchar(Alpha2);
+Alpha2++;
+}
+putchar ('\n');
+return (0);
+}
