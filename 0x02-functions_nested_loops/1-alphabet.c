bc534fe7e40000018025868a53bbff1e44373d04
MahaLaksmy <3354@holbertonschool.com>
2021-06-21 16:36:00 -0700

    Alphabet lowecase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int Alphabet = 97;
+
+while (Alphabet <= 122)
+{
+_putchar(Alphabet);
+Alphabet++;
+}
+_putchar ('\n');
+return (0);
+}
