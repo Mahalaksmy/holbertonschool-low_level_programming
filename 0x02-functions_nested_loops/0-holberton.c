f6459d7535b52b7a3c0dee3d488c74414622dfb5
MahaLaksmy <3354@holbertonschool.com>
2021-06-22 02:23:56 -0700

    functions

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 _putchar (72);
-_putchar (79);
-_putchar (76);
-_putchar (66);
-_putchar (69);
-_putchar (82);
-_putchar (84);
-_putchar (79);
-_putchar (78);
+_putchar (111);
+_putchar (108);
+_putchar (98);
+_putchar (101);
+_putchar (114);
+_putchar (116);
+_putchar (111);
+_putchar (110);
 
 _putchar ('\n');
 return (0);
