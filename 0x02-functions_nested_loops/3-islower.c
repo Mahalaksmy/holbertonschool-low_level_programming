07d1a7c7040a15f0832b3bfef2cef57d17903496
MahaLaksmy <3354@holbertonschool.com>
2021-06-22 06:37:46 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+
+#include "holberton.h"
+
+/**
+ *int _islower(int c)- check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
