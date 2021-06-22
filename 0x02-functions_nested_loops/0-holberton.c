13f9627544657fdc06637441745105ef9ac54e86
MahaLaksmy <3354@holbertonschool.com>
2021-06-22 02:17:25 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_putchar (72);
+_putchar (79);
+_putchar (76);
+_putchar (66);
+_putchar (69);
+_putchar (82);
+_putchar (84);
+_putchar (79);
+_putchar (78);
+
+_putchar ('\n');
+return (0);
+}
