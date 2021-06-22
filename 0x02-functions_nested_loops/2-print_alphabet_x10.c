328058a1cd89d764d19542e49610fe455db81194
MahaLaksmy <3354@holbertonschool.com>
2021-06-22 05:57:03 -0700

    add alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- *print_alphabet_10 - check the code.
+ *print_alphabet_x10 - check the code.
  *
  * Return: Always 0.
  */
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 int num = 0,
 Alphabet;
 
-while (num <= 10)
+while (num <= 9)
 {
 for (Alphabet = 97; Alphabet <= 122; Alphabet++)
 {
