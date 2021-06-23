e85a3f026b83202a535fe4ad9c97bec3ff730c90
MahaLaksmy <3354@holbertonschool.com>
2021-06-23 07:07:11 -0700

    sum the two variables

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *add -function that adds two integers and returns the result.
+ *@num1: Variable
+ *@num2: Variable
+ *Return: int
+ */
+int add(int num1, int num2)
+{
+int suma;
+suma = (num1 + num2);
+return (suma);
+}
