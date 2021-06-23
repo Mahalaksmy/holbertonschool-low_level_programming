55f189ed658f5807bbaf5c909c78311281c0953e
MahaLaksmy <3354@holbertonschool.com>
2021-06-23 06:44:28 -0700

    LastDigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *print_last_digit -Return the absolute value of an integer
+ *@num: Variable
+ *Return: int
+ */
+int print_last_digit(int num)
+{
+num %= 10;
+return (n);
+}
