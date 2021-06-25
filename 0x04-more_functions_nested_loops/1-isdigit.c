9a7d54b62f9e760aad247f339ab36bef189a2dbe
MahaLaksmy <3354@holbertonschool.com>
2021-06-25 01:44:04 -0700

    Write a function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _isdigit - Afunction that checks for a digit (0 through 9).
+ *_isdigit - Afunction that checks for a digit (0 through 9).
  *@c: Variable
  *Return: Always 1 or 0.
  */
@@ -13,5 +13,7 @@ if (c >= 0 && c <= 57)
 return (1);
 }
 else
+{
 return (0);
+}
 }
