d7ccf66dc949c35678747079a28df1ef1144299e
MahaLaksmy <3354@holbertonschool.com>
2021-06-25 02:18:08 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *print_numbers - Write a function that multiplies two integers.
+ *Return: int
+ */
+void print_numbers(void)
+{
+int numbers;
+for (numbers = 48; numbers <= 57; numbers++)
+{
+_putchar (numbers);
+}
+}
