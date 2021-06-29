475f66c20dfcbf04d9670bfa56065e06665ed825
MahaLaksmy <3354@holbertonschool.com>
2021-06-29 10:40:01 -0700

    reverese

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_rev - Write a function that prints a string, in reverse
+ *
+ * @s: variable
+ */
+void print_rev(char *s)
+{
+int x = 0;
+
+while (s[x] != '\0')
+x++;
+
+while (x >= 0)
+{
+_putchar(s[x]);
+x--;
+}
+_putchar('\n');
+}
