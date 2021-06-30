c9e6709708958ab5409d2ca9f4daf8eca6ea33d0
MahaLaksmy <3354@holbertonschool.com>
2021-06-30 13:26:40 -0700

    Write a function that copies the string pointed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strcpy -The same string
+ *
+ *@dest: Variable
+ *@src: Variable
+ */
+char *_strcpy(char *dest, char *src)
+{
+{
+int x;
+
+while (dest[x] != '\0')
+{
+x++;
+}
+x = 1;
+
+for (x = 0; src[x] != '\0'; x++)
+{
+dest[x] = src[x];
+}
+return (dest);
+}
+}
