25422f718eef984a462e5fcabe116765847b7f33
MahaLaksmy <3354@holbertonschool.com>
2021-07-05 08:19:03 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _memset - A a function that fills memory with a constant byte.
+ * @s: A pointer to the block of memory to fillt
+ * @b: is the character to fill s
+ * @n: Represents the size of the memory block to fill
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x;
+	char *ptr = s, a = b;
+
+	for (x = 0; x < n; x++)
+	{
+		ptr[x] = a;
+	}
+	return (ptr);
+}
