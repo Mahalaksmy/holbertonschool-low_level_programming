be2a79e000d69c9cfc6c61514f3368283320d336
MahaLaksmy <3354@holbertonschool.com>
2021-07-09 07:18:00 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - A function main
+ *@argc: Is a count of the arguments
+ *@argv: Is an array of pointers to the strings
+ *Return: Zero
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
