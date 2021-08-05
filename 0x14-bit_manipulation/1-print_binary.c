4913949850a76ddb61a9c682fa0bec99ddb6da52
Mahalaksmy <3354@holbertonschool.com>
2021-08-05 23:26:28 +0200

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,16 +4,9 @@
  * @n: Variable
  * Return: 0
  */
-void binary_printing(unsigned long int n)
+void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		return;
-
-	binary_printing(n >> 1);
-
-	if ((n & 1) == 1)
-		_putchar('1');
-
-	if ((n & 1) == 0)
-		_putchar('0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
