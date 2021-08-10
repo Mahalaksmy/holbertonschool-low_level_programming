a868d695be89b02daf0b9fdb41a768d577bf3f2a
Mahalaksmy <3354@holbertonschool.com>
2021-08-10 21:15:22 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,13 +11,13 @@ int create_file(const char *filename, char *text_content)
 {
 int fd, i;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
+	if (text_content == NULL)
 		return (1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd < 0)
 		return (-1);
 
