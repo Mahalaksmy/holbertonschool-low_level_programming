e8a553939269e03410c29b52d522c4b4781ce804
Mahalaksmy <3354@holbertonschool.com>
2021-08-10 18:49:44 +0200

    Create a function that creates a file.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file
+ * @filename: the file to read.
+ * @letters: is the number of letters it should read and print
+ * Return: Return Result of 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+ssize_t n_read, n_write;
+	char *buffer;
+
+	buffer = malloc(sizeof(buffer) * letters);
+	if (filename == NULL)
+		return (0);
+if (buffer == NULL)
+	return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	n_read = read(fd, buffer, letters);
+	if (fd < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	n_write = write(STDOUT_FILENO, buffer, n_read);
+	if (n_write < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (n_write);
+}
