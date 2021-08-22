4cf42feeb9149e417a9f7cc2c4b992396be9746c
Mahalaksmy <3354@holbertonschool.com>
2021-08-22 16:09:42 +0200

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - struct dog
+ * @d: Variable
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
