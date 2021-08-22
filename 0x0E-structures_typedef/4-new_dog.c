d976e17e7f589de92ecdd3ebbc016b7c78a530e5
Mahalaksmy <3354@holbertonschool.com>
2021-08-22 16:11:49 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,53 @@
 #include "dog.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * print_dog - struct dog
- * @d: Variable
+ * new_dog - create a new dog fuction
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog owner's name
+ *
+ * Return: A pointer to a new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
+	dog_t *new;
+	int name_new = 0, owner_new = 0, i;
 
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)";
+	for (; name[name_new]; name_new++)
+		;
+	for (; owner[owner_new]; owner_new++)
+		;
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->name = malloc(sizeof(char) * (name_new + 1));
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->owner = malloc(sizeof(char) * (owner_new + 1));
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; name[i]; i++)
+		new->name[i] = name[i];
+	new->name[i] = '\0';
+
+	for (i = 0; owner[i]; i++)
+		new->owner[i] = owner[i];
+	new->owner[i] = '\0';
+
+	new->age = age;
+
+	return (new);
 }
