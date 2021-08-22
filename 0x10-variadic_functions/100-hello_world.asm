23251e7c5693751e6696c2df523171bee1da9f8a
Mahalaksmy <3354@holbertonschool.com>
2021-08-22 16:22:47 +0200

    100-hello_world.asm

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,14 +1,14 @@
-		global    main
+global	main
 
-	        section   .text
-main:	    	mov       rax, 1	 ; system call for write
-	        mov       rdi, 1    ; the file handle 1 is stdout
-	        mov       rsi, holberton ;the address of string to output
-	        mov       rdx, 17	 ;the number of bytes in the string
-	        syscall		 ; invoke operating system to do the write
-	        mov       rax, 60 ; system call for exit
-	        xor       rdi, rdi	    ; the exit code 0
-	        syscall		    ; invoke operating system to exit
+	section	.text
+main:	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, holberton
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
 
-	        section   .data
-holberton:	db        "Hello, Holberton", 10 ; notice the newline at the end
+	section	.data
+holberton:db	"Hello, Holberton", 10
