1db3c3b7953a630fc38c7b5ed1d4fd4d5f5dbd21
Mahalaksmy <3354@holbertonschool.com>
2021-09-21 18:59:19 -0500

    Task 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * delete_dnodeint_at_index - Function that deletes the node at index of a DLL.
- * @head: Pointer to the head of the double linked list.
+ * @head: Pointer of the double linked list.
  * @index: index of the node that should be deleted. Index starts at 0
  * Return: 1 if it succeeded, -1 if it failed.
  */
