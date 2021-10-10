fecadc5208928871b7fcc68250597ebba2d78826
Mahalaksmy <3354@holbertonschool.com>
2021-10-09 19:15:43 -0500

    Task 1. djb2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,26 @@
 #include "hash_tables.h"
 
 /**
- *hash_table_t - Hash table data structure
- * @hash_table_create: a function that creates a hash table
+ * hash_table_create - A function that creates a hash table
  * @size: size of the hash table
- * @Return: New hash table or returns NULL
+ * Return: New hash table or  returns NULL
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
+	hash_table_t *new_table;
 
-    new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 
-    if (size <= 0)
-    {
-      return (NULL);
-    }
+	if (size <= 0)
+		return (NULL);
 
-    if (!new_table)
-    {
-        return (NULL);
-    }
+	if (!new_table)
+	{
+		return (NULL);
+	}
 
-    new_table->size = size;
+	new_table->size = size;
 	new_table->array = calloc(size, sizeof(hash_table_t *));
 	if (!new_table->array)
 	{
