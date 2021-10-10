942ca9d65b9bf3ce5a56c30de1e34ae643dcb49a
Maha Laksmy Amariles <85522251+Mahalaksmy@users.noreply.github.com>
2021-10-10 13:03:50 -0500

    changes in the code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - A function that creates a hash table
  * @size: size of the hash table
@@ -9,7 +8,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	HT = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 
 	if (size <= 0)
 		return (NULL);
