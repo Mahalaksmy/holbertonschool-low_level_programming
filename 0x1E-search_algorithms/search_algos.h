3201dca095cfe0b852e84e565741ecc0ea69b2cd
Mahalaksmy <3354@holbertonschool.com>
2022-01-19 23:06:54 +0100

    Task 1-binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,8 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, int start, int size);
+int binary_search(int *array, size_t size, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
